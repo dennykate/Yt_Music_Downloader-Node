ba16e621be126df30a543ca0a49c6c231481d6dd
Denny_Kate <dennykate22@gmail.com>
2023-11-25 00:53:18 +0630

    feat: add cors

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require("express");
 const ytdl = require("ytdl-core");
 const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
+app.use(
+  cors({
+    origin: ["*"],
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
